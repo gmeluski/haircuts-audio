6b0606c4ae4a8cf748fda16c71cefa8b910a0af9
Gregg Meluski <gmeluski@hotmail.com>
2015-09-28 08:00:05 -0400

    initial commit

---
+++ passport/init.js
@@ -0,0 +1,13 @@
+var User = ('../models/user');
+
+module.exports = function (passport) {
+  passport.serializeUser(function(user, done) {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+};
